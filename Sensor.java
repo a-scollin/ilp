214aa3abc2b5b3bcf60bcd62c4e1b241b89d7312
Andrew Scollin <andrewscollin@hotmail.co.uk>
2020-11-24 17:08:28 +0000

    Sensor comments

--- Sensor.java
+++ Sensor.java
@@ -6,6 +6,9 @@ public class Sensor extends Position {
 	private double battery;
 	private String reading;
 
+	
+	// Constructor method for Sensor, taking the position battery name and reading ..
+	
 	public Sensor(String name, Position sensorPosition, double battery, String reading) {
 		super(sensorPosition);
 		this.name = name;
@@ -13,23 +16,31 @@ public class Sensor extends Position {
 		this.reading = reading;
 	}
 
-	// Getters and toString
+	// Function for returning a string representation of the sensor 
 
 	public String toString() {
 		return "SENSOR: " + this.name + ", BATTERY : " + this.battery + " LOCATED @ " + super.toString();
 	}
 
+	// Function for returning the battery level of the Sensor
+
 	public double getBattery() {
 		return this.battery;
 	}
+	
+	// Function for returning the reading of the sensor
 
 	public String getReading() {
 		return this.reading;
 	}
+	
+	// Function for returning the position of the sensor
 
 	public Position getPosition() {
 		return this;
 	}
+	
+	// Function returning the name of the sensor
 
 	public String getName() {
 		return this.name;
